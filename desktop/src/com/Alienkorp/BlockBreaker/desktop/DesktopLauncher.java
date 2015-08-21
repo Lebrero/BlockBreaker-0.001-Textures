e45f1bde056be955f1f8d3256f943c07cd0d5ed5
Lebrero <david_lebrero@hotmail.com>
2015-08-21 20:26:57 +0200

    DLM-BlockBreaker-0.001 - Inicio

---
+++ desktop/src/com/Alienkorp/BlockBreaker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.Alienkorp.BlockBreaker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.Alienkorp.BlockBreaker.iniciarJuego;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new iniciarJuego(), config);
+	}
+}
