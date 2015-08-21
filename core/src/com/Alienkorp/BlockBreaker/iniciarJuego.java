4ebb87d5ed6348c1cd9b30223d85602678deddfa
Lebrero <david_lebrero@hotmail.com>
2015-08-22 01:44:07 +0200

    DLM-LibGDX-BlockBreaker-0.001

--- core/src/com/Alienkorp/BlockBreaker/iniciarJuego.java
+++ core/src/com/Alienkorp/BlockBreaker/iniciarJuego.java
@@ -1,19 +1,38 @@
 package com.Alienkorp.BlockBreaker;
 
+import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class iniciarJuego extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	int WINDOW_WIDTH = 1024;
+	 int WINDOW_HEIGHT = 768;
+	 Camera camera;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		
+	    if (Gdx.app.getType() == ApplicationType.Desktop) {
+	        int ANDROID_WIDTH = Gdx.graphics.getWidth();
+	        int ANDROID_HEIGHT = Gdx.graphics.getHeight();
+	        camera = new OrthographicCamera(ANDROID_WIDTH, ANDROID_HEIGHT);
+	     
+	        camera.update();
+	    } else {
+	        camera = new OrthographicCamera(WINDOW_WIDTH, WINDOW_HEIGHT);
+	      
+	        camera.update();
+	    }
+
+	
 	}
 
 	@Override
--- desktop/src/com/Alienkorp/BlockBreaker/desktop/DesktopLauncher.java
+++ desktop/src/com/Alienkorp/BlockBreaker/desktop/DesktopLauncher.java
@@ -5,8 +5,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.Alienkorp.BlockBreaker.iniciarJuego;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new iniciarJuego(), config);
+		
 	}
 }
